b345a87697b88447cf33c79bf6d533fdeddc5770
kingmaj0r <49806509+kingmaj0r@users.noreply.github.com>
2023-06-27 17:31:17 +0700

    Update colors.sh

--- colors.sh
+++ colors.sh
@@ -8,26 +8,26 @@ CYAN='\033[0;36m'
 ORANGE='\033[0;91m'
 NC='\033[0m' # No Color
 
-# Function to print a bordered line with larger text
+# Function to print a bordered line with random color
 print_bordered_line() {
     local text="$1"
     local text_length=${#text}
     local border_length=70
 
     local border_line=""
-    for i in $(seq 1 $border_length); do
+    for ((i=1; i<=border_length; i++)); do
         border_line="${border_line}─"
     done
 
     local padding_length=$(( (border_length - text_length - 2) / 2 ))
     local padding=""
-    for i in $(seq 1 $padding_length); do
+    for ((i=1; i<=padding_length; i++)); do
         padding="${padding} "
     done
 
     # Select a random color from the specified colors
     local colors=("$RED" "$GREEN" "$YELLOW" "$CYAN" "$ORANGE")
-    local random_color=$(shuf -e "${colors[@]}" -n 1)
+    local random_color="${colors[RANDOM % ${#colors[@]}]}"
 
     printf "%b┌%s┐%b\n" "$random_color" "$border_line" "$NC"
     printf "%b│%s%s%s   %b│%b\n" "$random_color" "$padding" "$text" "$padding" "$random_color" "$NC"
