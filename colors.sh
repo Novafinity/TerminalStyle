4f8af98b1b58f50d33a9822e4d2f3826ca95ce4b
kingmaj0r <49806509+kingmaj0r@users.noreply.github.com>
2023-06-27 17:54:09 +0700

    Update colors.sh

--- colors.sh
+++ colors.sh
@@ -8,28 +8,24 @@ CYAN='\033[0;36m'
 ORANGE='\033[0;91m'
 NC='\033[0m' # No Color
 
-# Function to print a bordered line with random color
+# Function to print a bordered line with larger text
 print_bordered_line() {
     local text="$1"
     local text_length=${#text}
     local border_length=70
 
     local border_line=""
-    for ((i=1; i<=border_length; i++)); do
+    for i in $(seq 1 $border_length); do
         border_line="${border_line}─"
     done
 
     local padding_length=$(( (border_length - text_length - 2) / 2 ))
     local padding=""
-    for ((i=1; i<=padding_length; i++)); do
+    for i in $(seq 1 $padding_length); do
         padding="${padding} "
     done
 
-    # Select a random color from the specified colors
-    local colors=("$RED" "$GREEN" "$YELLOW" "$CYAN" "$ORANGE")
-    local random_color="${colors[RANDOM % ${#colors[@]}]}"
-
-    printf "%b┌%s┐%b\n" "$random_color" "$border_line" "$NC"
-    printf "%b│%s%s%s   %b│%b\n" "$random_color" "$padding" "$text" "$padding" "$random_color" "$NC"
-    printf "%b└%s┘%b\n" "$random_color" "$border_line" "$NC"
+    printf "${GREEN}\e[1m┌%s┐${NC}\n" "$border_line"
+    printf "${GREEN}\e[1m│${NC}%s%s%s   ${GREEN}\e[1m│${NC}\n" "$padding" "$text" "$padding"
+    printf "${GREEN}\e[1m└%s┘${NC}\n" "$border_line"
 }
