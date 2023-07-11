e633a1454fc542acf79cca4c1864eb4402e3e69b
kingmaj0r <49806509+kingmaj0r@users.noreply.github.com>
2023-07-11 10:37:02 +0700

    Update colors.sh

--- colors.sh
+++ colors.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Text colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-CYAN='\033[0;36m'
-ORANGE='\033[0;91m'
-NC='\033[0m' # No Color
+# Function to generate a random color code
+generate_random_color() {
+    local colors=("\033[0;31m" "\033[0;32m" "\033[0;33m" "\033[0;36m" "\033[0;91m")
+    local color_count=${#colors[@]}
+    local random_index=$((RANDOM % color_count))
+    echo "${colors[random_index]}"
+}
 
 # Function to print a bordered line with larger text
 print_bordered_line() {
@@ -25,7 +25,9 @@ print_bordered_line() {
         padding="${padding} "
     done
 
-    printf "${GREEN}\e[1m┌%s┐${NC}\n" "$border_line"
-    printf "${GREEN}\e[1m│${NC}%s%s%s  ${GREEN}\e[1m│${NC}\n" "$padding" "$text" "$padding"
-    printf "${GREEN}\e[1m└%s┘${NC}\n" "$border_line"
+    local random_color=$(generate_random_color)
+
+    printf "${random_color}\e[1m┌%s┐${NC}\n" "$border_line"
+    printf "${random_color}\e[1m│${NC}%s%s%s  ${random_color}\e[1m│${NC}\n" "$padding" "$text" "$padding"
+    printf "${random_color}\e[1m└%s┘${NC}\n" "$border_line"
 }
