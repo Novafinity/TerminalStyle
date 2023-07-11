85eaa335cf61e7f7e9add536cdcae217a9b29887
kingmaj0r <49806509+kingmaj0r@users.noreply.github.com>
2023-07-11 17:29:46 +0700

    Create style.sh

---
+++ style.sh
@@ -0,0 +1,2 @@
+source <(curl -sSL "https://raw.githubusercontent.com/Novafinity/TerminalStyle/master/colors.sh")
+source <(curl -sSL "https://raw.githubusercontent.com/Novafinity/TerminalStyle/master/border.sh")
